b2694857aaaab3f956896042bdd7119794ab55ae
PangLAN <i@pll.moe>
2025-03-25 17:27:21 +0800

    add status:CFI

--- src/components/UpdateTableClient.tsx
+++ src/components/UpdateTableClient.tsx
@@ -134,7 +134,9 @@ const StatusCell = ({
         ? "bg-green-100 text-green-800 dark:bg-green-800 dark:text-green-100"
         : status === "CFH"
           ? "bg-red-100 text-red-800 dark:bg-red-800 dark:text-red-100"
-          : "bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-100"; // CFT and others
+          : status === "CFT"
+            ? "bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-100"
+            : "bg-yellow-100 text-yellow-800 dark:bg-yellow-800 dark:text-yellow-100";
 
   const statusElement = (
     <span
